2fc2cc08c9e713d62152a521790e088acebcfa93
kamran1137 <kami.c1301@gmail.com>
2024-01-29 16:40:53 +0500

    birthdays

---
+++ birthdays/src/List.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
